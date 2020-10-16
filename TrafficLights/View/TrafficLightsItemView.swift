b8d42d9b009d4199e6e8092e5d7d6dafd00a78ef
Oleg Tarasenko <69649259+olegtarasenkoGH@users.noreply.github.com>
2020-10-16 12:00:40 +0300

    comment

--- TrafficLights/View/TrafficLightsItemView.swift
+++ TrafficLights/View/TrafficLightsItemView.swift
@@ -22,7 +22,7 @@ struct CircleRedView: View {
             .frame(width: 140, height: 140, alignment: .center)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 7))
-            .shadow(color: Color(#colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)).opacity(0.5), radius: 15)
+            .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)).opacity(1.5), radius: 20)
             .padding(.top, 30)
     }
 }
@@ -36,7 +36,7 @@ struct CircleYellowView: View {
             .frame(width: 140, height: 140, alignment: .center)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 7))
-            .shadow(color: Color(#colorLiteral(red: 0.9529411793, green: 0.6862745285, blue: 0.1333333403, alpha: 1)).opacity(0.5), radius: 15)
+            .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)).opacity(1.5), radius: 20)
         
     }
 }
@@ -50,7 +50,7 @@ struct CircleGreenView: View {
             .frame(width: 140, height: 140, alignment: .center)
             .clipShape(Circle())
             .overlay(Circle().stroke(Color.white, lineWidth: 7))
-            .shadow(color: Color(#colorLiteral(red: 0.2745098174, green: 0.4862745106, blue: 0.1411764771, alpha: 1)).opacity(0.5), radius: 15)
+            .shadow(color: Color(#colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1)).opacity(1.5), radius: 20)
         
     }
 }
